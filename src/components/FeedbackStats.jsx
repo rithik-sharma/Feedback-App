ef74be7a02bd978f28139df392cf17bbecedac75
Rithik Sharma <srithik645@gmail.com>
2023-06-04 00:25:53 +0530

    Feedback form added !!

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,20 +1,24 @@
-import { useContext } from 'react'
-import FeedbackContext from '../context/FeedbackContext'
+import PropTypes from 'prop-types'
 
-function FeedbackStats() {
-    const { feedback } = useContext(FeedbackContext)
+function FeedbackStats({ feedback }) {
+    // Calculate ratings avg
+    let average =
+        feedback.reduce((acc, cur) => {
+            return acc + cur.rating
+        }, 0) / feedback.length
 
-    const average =
-        feedback.length === 0
-            ? 0
-            : feedback.reduce((acc, { rating }) => acc + rating, 0) / feedback.length
+    average = average.toFixed(1).replace(/[.,]0$/, '')
 
     return (
         <div className='feedback-stats'>
             <h4>{feedback.length} Reviews</h4>
-            <h4>Average Rating: {average.toFixed(1).replace(/[.,]0$/, '')}</h4>
+            <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
         </div>
     )
+}
+
+FeedbackStats.propTypes = {
+    feedback: PropTypes.array.isRequired,
 }
 
 export default FeedbackStats
